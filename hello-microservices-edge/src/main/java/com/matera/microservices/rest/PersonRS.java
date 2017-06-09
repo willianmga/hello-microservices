aa543e673ea27c1b5a6c6b8b0b36e4e1c15e6095
willianmga <willian-mga@hotmail.com>
2017-06-09 17:02:09 -0300

    task 11 created

---
+++ hello-microservices-edge/src/main/java/com/matera/microservices/rest/PersonRS.java
@@ -0,0 +1,15 @@
+package com.matera.microservices.rest;
+
+public class PersonRS {
+//	
+//	private final PersonService service;
+//	
+//	@Inject
+//	public PersonRS(PersonService service) {
+//		this.service = service;
+//	}
+//	
+//	
+//	
+
+}
