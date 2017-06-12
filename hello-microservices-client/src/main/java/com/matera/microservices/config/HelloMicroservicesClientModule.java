0a75dfaac592623a80c8fb08ef7f2164a6e9a113
willianmga <willian-mga@hotmail.com>
2017-06-12 14:40:02 -0300

    implemented client module class

--- hello-microservices-client/src/main/java/com/matera/microservices/config/HelloMicroservicesClientModule.java
+++ hello-microservices-client/src/main/java/com/matera/microservices/config/HelloMicroservicesClientModule.java
@@ -1,6 +1,7 @@
 package com.matera.microservices.config;
 
 import com.google.inject.AbstractModule;
+
 import com.google.inject.Provides;
 import com.google.inject.Scopes;
 import com.google.inject.Singleton;
@@ -10,6 +11,7 @@ import com.matera.microservices.api.rest.HystrixPersonClient;
 import com.netflix.client.ClientFactory;
 import com.netflix.niws.client.http.RestClient;
 
+@SuppressWarnings("deprecation")
 public class HelloMicroservicesClientModule extends AbstractModule {
 
 	@Override
