e90251220fc953a3ce203eabc16c59ea77568003
willianmga <willian-mga@hotmail.com>
2017-06-30 15:06:26 -0300

    implemented service register with eureka server

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -4,3 +4,6 @@ mvn clean install -DskipTests -o
 
 sudo docker build -t hellomicroservicesmiddle:latest hello-microservices-middle/
 sudo docker build -t hellomicroservicesedge:latest hello-microservices-edge/
+
+sudo docker tag hellomicroservicesmiddle willianmga/hellomicroservices:hellomicroservicesmiddle
+sudo docker tag hellomicroservicesedge willianmga/hellomicroservices:hellomicroservicesedge
--- hello-microservices-middle/src/main/java/com/matera/hellomicroservices/config/Bootstrap.java
+++ hello-microservices-middle/src/main/java/com/matera/hellomicroservices/config/Bootstrap.java
@@ -1,33 +1,25 @@
 package com.matera.hellomicroservices.config;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.inject.Binder;
+import com.netflix.karyon.server.ServerBootstrap;
 
-import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import com.google.inject.servlet.GuiceServletContextListener;
-
-public class Bootstrap extends GuiceServletContextListener {
+public class Bootstrap extends ServerBootstrap {
 
 	private static final String DB_STRATEGY = "db.strategy";
 	private static final String IN_MEMORY_STRATEGY = "in_memory";
 	
 	@Override
-	protected Injector getInjector() {
-	
-		List<AbstractModule> modules = new ArrayList<>();
-		modules.add(new RestModule());
-		modules.add(new HelloMicroservicesMiddleModule());
+	protected void configureBinder(Binder binder) {
+		
+		binder.install(new RestModule());
+		binder.install(new HelloMicroservicesMiddleModule());
 		
 		final String dbStrategy = System.getProperty(DB_STRATEGY);
 		if (IN_MEMORY_STRATEGY.equalsIgnoreCase(dbStrategy)) {
-			modules.add(new InMemoryStoreModule());
+			binder.install(new InMemoryStoreModule());
 		} else {
-			modules.add(new MySQLStoreModule());
+			binder.install(new MySQLStoreModule());
 		}
-		
-		return Guice.createInjector(modules);
 		
 	}
 
